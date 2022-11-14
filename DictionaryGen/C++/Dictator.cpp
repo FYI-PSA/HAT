70b2507ea8ef53c3df14e4485bc111d808163bdf
Funtime-UwU <Funtime.UwU.mail@gmail.com>
2022-11-14 17:12:49 +0330

    Updated C++ Version [still in development]

--- DictionaryGen/C++/Dictator.cpp
+++ DictionaryGen/C++/Dictator.cpp
@@ -1,17 +1,65 @@
 #include <iostream>
 #include <string.h>
+#include <string>
 #include <vector>
+
+using std::vector;
+using std::string;
+
+using std::getline;
+
+using std::endl;
+using std::cout;
+using std::cin;
+
+void InputCollector(void);
+void Printer(string text);
+void Printer(string text, bool newLine);
+void WordListCreatino(string baseString, int lengthVar);
 
 class Word_List
 {
     public:
-        std::string wordStart;
-        int listLength;
+        vector<string> list;
+        int size;
 };
+
+Word_List WordListObj;
 
 int main ()
 {
-    Word_List wordList;
-    std::cin >> wordList.wordStart;
-    std::cout << wordList.wordStart;
+    InputCollector();
+}
+
+void InputCollector()
+{
+    cout << "Enter input:\nEnter nothing in the input to stop.\n| >> " ;
+    string inputVar;
+    while(getline(cin,inputVar) && inputVar != "")
+    {
+        WordListObj.list.push_back(inputVar);
+        Printer("Successfully added "+inputVar+" to input list!");
+        inputVar = "";
+    }
+    Printer("Stopped adding entries to list.");
+    WordListObj.size = WordListObj.list.size();
+    cout << WordListObj.size << endl;
+}
+
+
+void Printer(string text)
+{
+    Printer(text, true);
+}
+
+void Printer(string text, bool newLine)
+{
+    cout << text;
+    if(newLine)
+        cout << endl;
+}
+
+void WordListCreation(string baseString, int lengthVar)
+{
+    bool flag = false;
 }
