cb9690f3f3bbf01dffc6e48eb8280f59133d5796
Funtime-UwU <Funtime.UwU.mail@gmail.com>
2022-11-16 00:16:24 +0330

    Version 0.1 of dictator is out! Has basic functionality for windows machines.

--- DictionaryGen/C++/Dictator.cpp
+++ DictionaryGen/C++/Dictator.cpp
@@ -1,17 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <Windows.h>
+#include <process.h>
+#include <Lmcons.h>
 #include <iostream>
+#include <stdlib.h>
 #include <string.h>
+#include <fstream>
 #include <string>
 #include <vector>
+
+using std::exit;
 
 using std::vector;
 using std::string;
 
 using std::getline;
 
+using std::fstream;
+using std::ofstream;
+
 using std::endl;
 using std::cout;
 using std::cin;
 
+void PathFinder(void);
+void WriteToFile(void);
 void InputCollector(void);
 void WordListCreation(string baseString, int lengthVar);
 
@@ -20,20 +34,29 @@ class Word_List
     public:
         vector<string> wordList;
         int wordListSize;
+
+        string filePath;
+
+        vector<string> finalWordList;
+        int finalWordListSize;
 };
 
 Word_List WordListObj;
 
 int main ()
 {
-    cout << "[#!] Dictator V0.1" << endl << endl;
+    cout << "[*] Dictator V0.1" << endl << endl;
     InputCollector();
-    cout << "[$!] Analyzing entries..." << endl << endl;
-    cout << "[$!] Creating word list..." << endl;
+    cout << "[!] Analyzing entries..." << endl << endl;
+    cout << "[!] Creating word list..." << endl;
+    PathFinder();
     string based = "";
-    int maxLength = 3;
+    int maxLength = 4;
     WordListCreation(based, maxLength);
-    cout << endl << "[$!$] Done!" << endl;
+    cout << "[!] Writing to file..." << endl;
+    WordListObj.finalWordListSize = WordListObj.finalWordList.size();
+    WriteToFile();
+    cout << endl << "[$] Done!" << endl;
 }
 
 void InputCollector()
@@ -51,6 +74,32 @@ void InputCollector()
     }
     cout << "[/] Stopped adding entries to list." << endl;
     WordListObj.wordListSize = WordListObj.wordList.size();
+}
+
+void PathFinder(void)
+{
+    char userName[UNLEN+1];
+    DWORD userNameLength = UNLEN+1;
+    GetUserName(userName, &userNameLength);
+    string homePath = "C:/Users/" + (string)userName + "/Documents/Hackers-Toolbox/Dictionaries/";
+    string fileName;
+    cout << "[@] Name the file to save your dictionary as"
+    << endl << "[!] (Files will be saved at '" + homePath + "' as a .txt file )"
+    << endl << "[?] File name : ";
+    getline(cin,fileName);
+    if (fileName == "")
+        fileName = "dictionary.txt";
+    else
+        fileName = fileName+".txt";
+    WordListObj.filePath = homePath + fileName;
+    if (CreateDirectory(homePath.c_str(), NULL) || ERROR_ALREADY_EXISTS == GetLastError())
+        cout << "[$] Successfully created file '" + WordListObj.filePath + "' !" << endl;
+    else
+    {
+        cout << "[#] ERR : COULDN'T CREATE FILE '" + WordListObj.filePath + "' !"
+        << endl << "[!] ENDING PROGRAM [!]";
+        exit(1);
+    }
 }
 
 void WordListCreation(string baseString, int lengthVar)
@@ -71,7 +120,19 @@ void WordListCreation(string baseString, int lengthVar)
         }
         else
         {
-            cout << newString << endl;
+            WordListObj.finalWordList.push_back(newString+"\n");
         }
     }
+}
+
+void WriteToFile(void)
+{
+    fstream fileObj(WordListObj.filePath);
+    string listItem;
+    for (int finalIndex = 0; finalIndex < WordListObj.finalWordListSize; finalIndex++)
+    {
+        listItem = WordListObj.finalWordList.at(finalIndex);
+        fileObj << listItem;
+    }
+    fileObj.close();
 }
