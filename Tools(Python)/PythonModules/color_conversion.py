97b758655d43bce7ba1d2fc28b01203ce4cd2d02
Funtime-UwU <Funtime.UwU.mail@gmail.com>
2023-02-15 23:31:20 +0330

    Started work on JPG

---
+++ Tools(Python)/PythonModules/color_conversion.py
@@ -0,0 +1,21 @@
+# RGB will be a central point.
+# For transformation of formats that aren't RGB, first turn into RGB, then your desired format.
+# Not gonna make this file 2 megabytes of every single case of conversion lol.
+
+def YCbCr_to_RGB():
+    pass
+
+def RGB_to_YCbCr():
+    pass
+
+def CMYK_to_RGB():
+    pass
+
+def RBG_to_CMYK():
+    pass
+
+def HSL_to_RGB():
+    pass
+
+def RGB_to_HSL():
+    pass
---
+++ Tools(Python)/jpg.py
@@ -0,0 +1,4 @@
+from PythonModules import file_modifying_module, chunks_module, color_conversion
+
+jpg_img = "Assets/jpg_test.jpg"
+file_modifying_module.read_bytes_data(jpg_img)
